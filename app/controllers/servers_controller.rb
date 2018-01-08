a49a7bb47c60caf0ca94551c82c15ec62967a157
Richard J Hancock <rjhancock@tapenvy.us>
2018-01-08 09:07:05 -0600

    Adjustments related to CC.

--- app/controllers/servers_controller.rb
+++ app/controllers/servers_controller.rb
@@ -1,6 +1,4 @@
 class ServersController < ApplicationController
-  before_action :set_server, only: [:update, :destroy]
-
   # GET /servers
   # GET /servers.json
   def index
@@ -10,7 +8,7 @@ class ServersController < ApplicationController
   # POST /servers.json
   def create
     @server = Server.new(server_params)
-    @server.ip_address = remote_ip()
+    @server.ip_address = remote_ip
 
     if @server.save
       render json: @server, status: :created
@@ -21,7 +19,7 @@ class ServersController < ApplicationController
 
   # PATCH/PUT /servers/1.json
   def update
-    if @server.update(server_params)
+    if server.update(server_params)
       head :ok
     else
       render json: @server.errors, status: :unprocessable_entity
@@ -30,18 +28,16 @@ class ServersController < ApplicationController
 
   # DELETE /servers/1.json
   def destroy
-    @server.destroy
+    server.destroy
     head :ok
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_server
-      @server = Server.where(server_key: params[:id]).first
-    end
+  def server
+    @server ||= Server.where(server_key: params[:id]).first
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def server_params
-      params.require(:server).permit(:port, :passworded, :users)
-    end
+  def server_params
+    params.require(:server).permit(:port, :passworded, :users)
+  end
 end
