c7fbc356b74412316cfa3e7064b2be67dfa04b06
Richard J Hancock <rjhancock@tapenvy.us>
2018-01-11 04:13:19 -0600

    Updated server display.

--- spec/views/servers/index.html.erb_spec.rb
+++ spec/views/servers/index.html.erb_spec.rb
@@ -24,9 +24,10 @@ RSpec.describe "servers/index", type: :view do
 
   it "renders a list of servers" do
     render
-    assert_select "tr>td", :text => 2345.to_s, :count => 2
-    assert_select "tr>td", :text => "127.0.0.1".to_s, :count => 2
-    assert_select "tr>td", :text => false.to_s, :count => 2
+    assert_select "tr>td", :text => "127.0.0.1:2345".to_s, :count => 2
+    assert_select "tr>td", :text => 'No', :count => 2
     assert_select "tr>td", :text => "MyText".to_s, :count => 2
+    assert_select "tr>td", :text => "0.43-git".to_s, :count => 2
+    assert_select "tr>td", :text => "Launch".to_s, :count => 2
   end
 end
