032f722cb2e1f0f460108795f7eecd9c2701f324
Richard J Hancock <rjhancock@tapenvy.us>
2018-01-21 16:26:16 -0600

    Fixed issue in travis.yml and 2 issues found via CC

--- app/controllers/servers_controller.rb
+++ app/controllers/servers_controller.rb
@@ -38,6 +38,12 @@ class ServersController < ApplicationController
   end
 
   def server_params
-    params.require(:server).permit(:port, :passworded, :users, :version, :phase, :motd)
+    params.require(:server).permit(
+      :port,
+      :passworded,
+      :users,
+      :version,
+      :phase,
+      :motd)
   end
 end
