859d4859e5a66bc27e5c5d4f7c403900d64960e0
Richard J Hancock <rjhancock@tapenvy.us>
2018-01-21 20:58:54 -0600

    Added announce url for older clients.

--- app/controllers/servers_controller.rb
+++ app/controllers/servers_controller.rb
@@ -17,6 +17,29 @@ class ServersController < ApplicationController
     end
   end
 
+  # POST /servers/announce
+  def announce
+    server_params = {
+      ip_address: remote_ip,
+      port:       params[:port],
+      version:    params[:version]
+    }
+
+    server_params[:users]       = params[:players].join(', ') if params[:players].present?
+    server_params[:phase]       = params[:close].present? ? 'Closed' : 'Lobby'
+    server_params[:passworded]  = params[:pw].present?
+
+    if params[:key].present? && Server.exists?(server_key: params[:key])
+      server = Server.where(server_key: params[:key]).first
+      server.update_attributes(server_params)
+    else
+      server = Server.new(server_params)
+    end
+
+    server.save
+    render plain: server.server_key, layout: false
+  end
+
   # PATCH/PUT /servers/1.json
   def update
     if server.update(server_params)
