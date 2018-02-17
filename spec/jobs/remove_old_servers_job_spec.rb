3141c568f0372e585bf652b3dcbc37cb40b7f7eb
Richard Hancock <rjhancock@tapenvy.us>
2018-02-17 13:50:23 -0600

    Updated spec to 100% coverage.

--- spec/jobs/remove_old_servers_job_spec.rb
+++ spec/jobs/remove_old_servers_job_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe RemoveOldServersJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  include ActiveJob::TestHelper
+
+  subject(:job) { described_class.perform_later }
+
+  it 'queues the job' do
+    expect { job }
+      .to change(ActiveJob::Base.queue_adapter.enqueued_jobs, :size).by(1)
+  end
+
+  it 'is in urgent queue' do
+    expect(RemoveOldServersJob.new.queue_name).to eq('default')
+  end
+
+  it 'executes perform' do
+    expect(Server).to receive(:where)
+    perform_enqueued_jobs { job }
+  end
+
+  after do
+    clear_enqueued_jobs
+    clear_performed_jobs
+  end
 end
