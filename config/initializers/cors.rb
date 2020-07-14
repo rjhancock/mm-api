8e1b4f00856270730de1432f2b6cc95c59e55576
Richard J Hancock <rjhancock@tapenvy.us>
2020-07-13 19:31:16 -0500

    Swith to ANY method

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.middleware.insert_before 0, Rack::Cors do
   allow do
      origins '*'
-     resource '/servers.js', headers: :any, :methods => [:get]
-   end
+     resource '/servers.js', headers: :any, methods: :any
+  end
 end
